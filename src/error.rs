6dd6073782a4e5bcd75f1cb97468bf4bad9abbf7
Matt Klein <mattklein123@gmail.com>
2024-08-19 10:21:58 -0600

    various changes (#1)
    1) chrono -> time
    2) allow time to be passed in for external time control
    3) Return error if there is a sequence overflow so caller can handle
    4) Use full 64 bits and extend the sequence to 9 bits.
    
    Signed-off-by: Matt Klein <mklein@bitdrift.io>

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
 use std::error::Error as StdError;
 use thiserror::Error;
+use time::OffsetDateTime;
 
 /// Convenience type alias for usage within sonyflake.
 pub(crate) type BoxDynError = Box<dyn StdError + 'static + Send + Sync>;
@@ -9,15 +9,15 @@ pub(crate) type BoxDynError = Box<dyn StdError + 'static + Send + Sync>;
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("start_time `{0}` is ahead of current time")]
-    StartTimeAheadOfCurrentTime(DateTime<Utc>),
+    StartTimeAheadOfCurrentTime(OffsetDateTime),
     #[error("machine_id returned an error: {0}")]
     MachineIdFailed(#[source] BoxDynError),
     #[error("check_machine_id returned false")]
     CheckMachineIdFailed,
     #[error("over the time limit")]
     OverTimeLimit,
+    #[error("over the sequence limit")]
+    OverSequenceLimit,
     #[error("could not find any private ipv4 address")]
     NoPrivateIPv4,
-    #[error("mutex is poisoned (i.e. a panic happened while it was locked)")]
-    MutexPoisoned,
 }
